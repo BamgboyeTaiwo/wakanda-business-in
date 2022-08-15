8cbe0b9430f823ccb519a7e43a0850922283e86d
rukysandy <rukysandy57@gmail.com>
2022-08-15 11:57:51 +0100

    refactored footer

--- components/lpage/ebook.js
+++ components/lpage/ebook.js
@@ -4,7 +4,7 @@ import styles from "./ebook.module.css";
 
 export default function Ebook() {
   return (
-    <div className="container py-32 sm:pb-96 md:pb-32 grid grid-cols-1 md:grid-cols-2 content-center justify-items-center gap-2">
+    <div className="container py-32 sm:pb-96 md:pb-32 grid grid-cols-1 md:grid-cols-2 content-center justify-items-center gap-2 sm:mb-16">
       <div>
         <Image src="/e-book.png" alt="E-book" width={472} height={460} />
       </div>
